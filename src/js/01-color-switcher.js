92a9b67f7d9816ee3a7e626979e7ad49f808cc71
Shegda Igor <igorbeef@gmail.com>
2022-11-21 21:42:18 +0200

    switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+btnStart = document.querySelector('button[data-start]');
+btnStop = document.querySelector('button[data-stop]');
 
+let intervalId = null;
+
+btnStart.addEventListener('click', startBtnHandler);
+btnStop.addEventListener('click', stopBtnHandler);
+btnStop.setAttribute('disabled', true);
+
+function startBtnHandler() {
+  intervalId = setInterval(getBodyColor, 1000);
+  btnStop.removeAttribute('disabled');
+  btnStart.setAttribute('disabled', true);
+}
+
+function stopBtnHandler() {
+  clearInterval(intervalId);
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', true);
+}
+
+function getBodyColor() {
+  return (document.body.style.backgroundColor = getRandomHexColor());
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
